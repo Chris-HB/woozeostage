c8c336307a3d025a41eb1e6867d86b50bbcf2f01
pc8 <pc8@192.168.1.22>
2014-09-24 09:36:30 +0200

    mise à jour Chat

--- web/js/chat.js
+++ web/js/chat.js
@@ -1,23 +1,53 @@
 $(document).ready(function() {
     var box = null;
-    $("#userclick span").click(function() {
+    $("#userclick li").click(function() {
         var $username = $(this).text();
-        //var $username = $("#userclick li").data("userclick");
-        //var $username = event.srcElement;
-        //alert($username);
         var pseudo = ($("#pseudo").data("pseudo"));
-
-        if (box) {
-            box.chatbox("option", "boxManager").toggleBox();
-        }
-        else {
-            box = $("#chat_div").chatbox({id: "chat_div",
-                user: {key: "value"},
+        var $container = $("#chat_div");
+        var $divexiste = false;
+        // ----------
+        // Affichage
+        // ----------
+        //
+        // on teste si un div ayant un id du même nom que le username existe déjà
+//        $('#chat_div>div[id]').each(function() {
+//
+//            if ($(this).attr('id') == $username) {
+//                $divexiste = true;
+//            }
+//            alert('toto - ' + $divexiste);
+//        });
+        $($container.attr('id')).each(function() {
+            alert($(this).attr('id'));
+        });
+// ---
+// si il n'existe pas, je le créer (id=username)
+// ---
+        if (!$divexiste) {
+            $container.append('<div id="' + $username + '" style="float :left;"></div>');
+            //
+            // je crée une box
+            box = $('#' + $username).chatbox({id: $username,
                 title: "woozeostage chat : " + $username,
+                offset: 20,
+                user: {key: "value"},
                 messageSent: function(id, user, msg) {
                     $("#log").append(id + " said: " + msg + "<br/>");
-                    $("#chat_div").chatbox("option", "boxManager").addMsg(id, msg);
+                    $('#' + $username).chatbox("option", "boxManager").addMsg(pseudo, msg);
                 }});
         }
+        else {
+//
+// je crée une box
+            box = $('#' + $username).chatbox({id: $username,
+                title: "woozeostage chat : " + $username,
+                offset: 20,
+                user: {key: "value"},
+                messageSent: function(id, user, msg) {
+                    $("#log").append(id + " said: " + msg + "<br/>");
+                    $('#' + $username).chatbox("option", "boxManager").addMsg(pseudo, msg);
+                }});
+        }
+
     });
 });
