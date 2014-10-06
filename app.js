06a009e03d27ef724a5c1ac81d671ac594532c1d
pc8 <pc8@192.168.1.22>
2014-10-06 12:11:34 +0200

    rien

--- app.js
+++ app.js
@@ -1,36 +1,36 @@
 var http = require('http');
 var fs = require('fs');
+
 // Chargement du fichier index.html affiché au client
 var server = http.createServer(function(req, res) {
+    //fs.readFile('./index.html', 'utf-8', function (error, content) {
     fs.readFile('./index.html', 'utf-8', function(error, content) {
         res.writeHead(200, {"Content-Type": "text/html"});
         res.end(content);
     });
 });
+
 // Chargement de socket.io
 var io = require('socket.io').listen(server);
+
 io.sockets.on('connection', function(socket, pseudo) {
     // Quand on client se connecte, on lui envoie un message
     socket.emit('message', 'Vous êtes bien connecté !');
-
     // On signale aux autres clients qu'il y a un nouveau venu
     socket.broadcast.emit('message', 'Un autre client vient de se connecter ! ');
 
     // Dès qu'on nous donne un pseudo, on le stocke en variable de session
     socket.on('petit_nouveau', function(pseudo) {
-        //socket.set('pseudo', pseudo);
         socket.pseudo = pseudo;
     });
 
     // Dès qu'on reçoit un "message" (clic sur le bouton), on le note dans la console
-    socket.on('message', function(message) {
-        // On récupère le pseudo de celui qui a cliqué dans les variables de session
+    socket.on('message', function(mes) {
         pseudo = socket.pseudo;
-        socket.emit(message, document.getElementById('reponse').val());
+        socket.emit('mes', pseudo + ' ' + mes);
         //console.log(pseudo + ' me parle ! Il me dit : ' + message);
-//        socket.get('pseudo', function(error, pseudo) {
-//            console.log(pseudo + ' me parle ! Il me dit : ' + message);
-//        });
     });
 });
+
+
 server.listen(8080);
